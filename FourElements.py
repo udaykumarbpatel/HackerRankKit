e57dfe4731a34f62e7ea2f26896251417025c08e
Uday Patel <udaykumarbpatel@gmail.com>
2018-08-29 08:06:41 -0700

    Update-Commit

---
+++ FourElements.py
@@ -0,0 +1,21 @@
+# 1
+# 6
+# 1 5 1 0 6 0
+# 7
+
+T = int(raw_input())
+
+
+def sum_of_four_num(n,a, X):
+
+    pass
+
+
+for i in xrange(T):
+    n = int(raw_input())
+    a = map(int, raw_input().rstrip().split())
+    X = int(raw_input())
+    sum_of_four_num(n,a,X)
+
+
+
