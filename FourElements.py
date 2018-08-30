280beb32457745e20fa323a19cbfe586ea406ac9
ukumar <udaykumarbpatel@gmail.com>
2018-08-29 18:41:25 -0700

    Update-Commit

--- FourElements.py
+++ FourElements.py
@@ -3,19 +3,52 @@
 # 1 5 1 0 6 0
 # 7
 
-T = int(raw_input())
+class Aux_Item(object):
+    first = 0
+    second = 0
+    sum = 0
+
+    def __init__(self, first, second, sum):
+        self.first = first
+        self.second = second
+        self.sum = sum
 
 
-def sum_of_four_num(n,a, X):
+def make_item(first, second, sum):
+    item = Aux_Item(first, second, sum)
+    return item
+
+
+def aux_print(aux):
+    for item in aux:
+        print item.first, item.second, item.sum
+
+def sum_of_four(arr, n, X):
+    aux = []
+    for i in range(0, n, 1):
+        for j in range(i, n, 1):
+            if i != j:
+                aux.append(make_item(i, j, arr[i] + arr[j]))
+
+    aux_print(aux)
+
+    for i in range(0, len(aux), 1):
+        f_start = aux[i].first
+        s_start = aux[i].second
+        u_start = aux[i].sum
+
+        diff = X - aux[i].sum
+        if diff in aux
+
+
 
     pass
 
+
+T = int(raw_input())
 
 for i in xrange(T):
     n = int(raw_input())
     a = map(int, raw_input().rstrip().split())
     X = int(raw_input())
-    sum_of_four_num(n,a,X)
-
-
-
+    sum_of_four(a, n, X)
